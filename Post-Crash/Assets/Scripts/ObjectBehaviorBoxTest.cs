02d9210ca757d5795d4d297c6b59694ece8cce0c
ImhotepwasinAmerica <potiskandrew8@gmail.com>
2019-11-30 16:46:45 -0600

    Mirror built, BoxTest partially

---
+++ Post-Crash/Assets/Scripts/ObjectBehaviorBoxTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBehaviorBoxTest : ObjectBehaviorDefault
+{
+    public GameObject held_object_anchor;
+
+    public void DestroyOnDeath()
+    {
+        int health = object_data.health;
+
+        if (health < 0)
+        {
+            //data_container.GetComponent<DataContainer>().saved_objects.Remove(object_data);
+            Object.Destroy(object_in_question);
+        }
+    }
+
+    public override void UseDefault(GameObject new_anchor)
+    {
+        if (held_object_anchor == null)
+        {
+            held_object_anchor = new_anchor;
+            held_object_anchor.transform.localRotation = new Quaternion(0, 0, 0, 0);
+
+            this.GetComponent<Rigidbody>().useGravity = false;
+            this.GetComponent<Rigidbody>().freezeRotation = true;
+            Debug.Log("Attach " + held_object_anchor);
+        }
+        else
+        {
+            held_object_anchor.transform.localRotation = new Quaternion(0, 0, 0, 0);
+
+            this.GetComponent<Rigidbody>().useGravity = true;
+            this.GetComponent<Rigidbody>().freezeRotation = false;
+            held_object_anchor = null;
+            Debug.Log("Detach " + held_object_anchor);
+        }
+    }
+
+    public override void MoveAugment()
+    {
+        if (held_object_anchor != null)
+        {
+            this.transform.position = Vector3.Lerp(this.transform.position, held_object_anchor.transform.position, 20f * Time.deltaTime);
+
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, held_object_anchor.transform.localRotation, 20f * Time.deltaTime);
+        }
+    }
+}
