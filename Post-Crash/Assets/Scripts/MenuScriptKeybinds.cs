e475f78ea84b51c4a695f68133476f6513a887d1
ImhotepwasinAmerica <potiskandrew8@gmail.com>
2020-03-01 14:05:16 -0600

    Automatic Keybinds Menu Generation
    The elements in the keybinding menu will now be generated automatically using an array of strings, each string being a game action.

--- Post-Crash/Assets/Scripts/MenuScriptKeybinds.cs
+++ Post-Crash/Assets/Scripts/MenuScriptKeybinds.cs
@@ -6,7 +6,9 @@ using System.IO;
 
 public class MenuScriptKeybinds : MonoBehaviour
 {
-    public GameObject keybindings_menu, options_menu, data_container;
+    public GameObject keybindings_menu, options_menu, data_container, content;
+
+    public string[] action_array;
 
     private GameObject current_button;
 
@@ -21,6 +23,8 @@ public class MenuScriptKeybinds : MonoBehaviour
         quicksave, quickload, pause, log, objectives, stats,
         gamma_up, gamma_down, item_rotate;
 
+    Button butt;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,44 +33,72 @@ public class MenuScriptKeybinds : MonoBehaviour
             DeveloperPreferences.Keybinds();
         }
 
-        action_general.text = PlayerPrefs.GetString("General Action");
-        toolbelt_one.text = PlayerPrefs.GetString("Toolbelt One");
-        toolbelt_two.text = PlayerPrefs.GetString("Toolbelt Two");
-        toolbelt_three.text = PlayerPrefs.GetString("Toolbelt Three");
-        toolbelt_four.text = PlayerPrefs.GetString("Toolbelt Four");
-        toolbelt_five.text = PlayerPrefs.GetString("Toolbelt Five");
-        toolbelt_six.text = PlayerPrefs.GetString("Toolbelt Six");
-        toolbelt_seven.text = PlayerPrefs.GetString("Toolbelt Seven");
-        toolbelt_eight.text = PlayerPrefs.GetString("Toolbelt Eight");
-        toolbelt_nine.text = PlayerPrefs.GetString("Toolbelt Nine");
-        toolbelt_ten.text = PlayerPrefs.GetString("Toolbelt Ten");
-        attack_primary.text = PlayerPrefs.GetString("Primary Attack");
-        attack_secondary.text = PlayerPrefs.GetString("Secondary Attack");
-        attack_tertiary.text = PlayerPrefs.GetString("Tertiary Attack");
-        kick.text = PlayerPrefs.GetString("Kick");
-        block.text = PlayerPrefs.GetString("Block");
-        move_forward.text = PlayerPrefs.GetString("Move Forward");
-        move_backward.text = PlayerPrefs.GetString("Move Backward");
-        move_left.text = PlayerPrefs.GetString("Move Left");
-        move_right.text = PlayerPrefs.GetString("Move Right");
-        jump.text = PlayerPrefs.GetString("Jump");
-        squat.text = PlayerPrefs.GetString("Squat");
-        speed_toggle.text = PlayerPrefs.GetString("Speed Toggle");
-        toolbelt_scroll_up.text = PlayerPrefs.GetString("Toolbelt Scroll Up");
-        toolbelt_scroll_down.text = PlayerPrefs.GetString("Toolbelt Scroll Down");
-        drop.text = PlayerPrefs.GetString("Drop");
-        quicksave.text = PlayerPrefs.GetString("Quicksave");
-        quickload.text = PlayerPrefs.GetString("Quickload");
-        pause.text = PlayerPrefs.GetString("Pause");
-        log.text = PlayerPrefs.GetString("Log");
-        objectives.text = PlayerPrefs.GetString("Objectives");
-        stats.text = PlayerPrefs.GetString("Stats");
-        gamma_up.text = PlayerPrefs.GetString("Gamma Up");
-        gamma_down.text = PlayerPrefs.GetString("Gamma Down");
-        item_rotate.text = PlayerPrefs.GetString("Item Rotate");
-        lean_left.text = PlayerPrefs.GetString("Lean Left");
-        lean_right.text = PlayerPrefs.GetString("Lean Right");
+        //action_general.text = PlayerPrefs.GetString("General Action");
+        //toolbelt_one.text = PlayerPrefs.GetString("Toolbelt One");
+        //toolbelt_two.text = PlayerPrefs.GetString("Toolbelt Two");
+        //toolbelt_three.text = PlayerPrefs.GetString("Toolbelt Three");
+        //toolbelt_four.text = PlayerPrefs.GetString("Toolbelt Four");
+        //toolbelt_five.text = PlayerPrefs.GetString("Toolbelt Five");
+        //toolbelt_six.text = PlayerPrefs.GetString("Toolbelt Six");
+        //toolbelt_seven.text = PlayerPrefs.GetString("Toolbelt Seven");
+        //toolbelt_eight.text = PlayerPrefs.GetString("Toolbelt Eight");
+        //toolbelt_nine.text = PlayerPrefs.GetString("Toolbelt Nine");
+        //toolbelt_ten.text = PlayerPrefs.GetString("Toolbelt Ten");
+        //attack_primary.text = PlayerPrefs.GetString("Primary Attack");
+        //attack_secondary.text = PlayerPrefs.GetString("Secondary Attack");
+        //attack_tertiary.text = PlayerPrefs.GetString("Tertiary Attack");
+        //kick.text = PlayerPrefs.GetString("Kick");
+        //block.text = PlayerPrefs.GetString("Block");
+        //move_forward.text = PlayerPrefs.GetString("Move Forward");
+        //move_backward.text = PlayerPrefs.GetString("Move Backward");
+        //move_left.text = PlayerPrefs.GetString("Move Left");
+        //move_right.text = PlayerPrefs.GetString("Move Right");
+        //jump.text = PlayerPrefs.GetString("Jump");
+        //squat.text = PlayerPrefs.GetString("Squat");
+        //speed_toggle.text = PlayerPrefs.GetString("Speed Toggle");
+        //toolbelt_scroll_up.text = PlayerPrefs.GetString("Toolbelt Scroll Up");
+        //toolbelt_scroll_down.text = PlayerPrefs.GetString("Toolbelt Scroll Down");
+        //drop.text = PlayerPrefs.GetString("Drop");
+        //quicksave.text = PlayerPrefs.GetString("Quicksave");
+        //quickload.text = PlayerPrefs.GetString("Quickload");
+        //pause.text = PlayerPrefs.GetString("Pause");
+        //log.text = PlayerPrefs.GetString("Log");
+        //objectives.text = PlayerPrefs.GetString("Objectives");
+        //stats.text = PlayerPrefs.GetString("Stats");
+        //gamma_up.text = PlayerPrefs.GetString("Gamma Up");
+        //gamma_down.text = PlayerPrefs.GetString("Gamma Down");
+        //item_rotate.text = PlayerPrefs.GetString("Item Rotate");
+        //lean_left.text = PlayerPrefs.GetString("Lean Left");
+        //lean_right.text = PlayerPrefs.GetString("Lean Right");
 
+
+
+
+
+
+        action_array = new string[] { "General Action", "Toolbelt One", "Toolbelt Two", "Toolbelt Three", "Toolbelt Four", "Toolbelt Five", "Toolbelt Six", "Toolbelt Seven",
+            "Toolbelt Eight", "Toolbelt Nine","Toolbelt Ten", "Primary Attack", "Secondary Attack", "Tertiary Attack", "Kick", "Block", "Move Forward", "Move Backward", "Move Left", "Move Right",
+            "Jump", "Squat", "Speed Toggle", "Toolbelt Scroll Up", "Toolbelt Scroll Down", "Drop", "Quicksave", "Quickload", "Pause", "Log", "Objectives", "Stats", "Gamma Up", "Gamma Down",
+            "Item Rotate", "Lean Left", "Lean Right"};
+
+        float y_position = 0;
+
+        foreach (string action in action_array)
+        {
+            GameObject keybinder = Instantiate(Resources.Load<GameObject>("Keybind Template"), content.GetComponent<RectTransform>().position, new Quaternion(0,0,0,0));
+            
+            keybinder.transform.parent = content.transform;
+
+            keybinder.GetComponent<RectTransform>().position += new Vector3(241.5f,-21.65f-y_position,0);
+
+            y_position += 50.0f;
+
+            keybinder.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = action;
+
+            keybinder.transform.GetChild(1).GetChild(0).GetComponent<Text>().text = PlayerPrefs.GetString(action);
+
+            keybinder.transform.GetChild(1).GetComponent<Button>().onClick.AddListener(delegate() { DoBoth(action, keybinder.transform.GetChild(1).gameObject); });
+        }
     }
 
     // Update is called once per frame
@@ -140,6 +172,13 @@ public class MenuScriptKeybinds : MonoBehaviour
     public void SetCurrentAction(string action)
     {
         current_action = action;
+    }
+
+    public void DoBoth(string action, GameObject clicked)
+    {
+        ChangeKey(clicked);
+        SetCurrentAction(action);
+
     }
 
     public void Open_Menu_Page(GameObject page)
